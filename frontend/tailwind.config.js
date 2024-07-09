48d733ceee93af325f9e3bd855a7dc34207f34ec
4lysson-a <4ly.alcantara@gmail.com>
2024-07-08 23:35:42 -0300

    chore: Add configuration files for Vercel, PostCSS, jsconfig, Babel, Vite, and ESLint

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		screens: {
+			xm: '340px'
+		},
+		extend: {
+			colors: {
+				primary: 'var(--primary)',
+				texts: 'var(--texts)',
+				card: 'var(--card)',
+				background: 'var(--background)',
+				colorBorder: 'var(--color-border)',
+				colorBorderContrsat: 'var(--color-border-contrast)',
+				floatingButtonBackground: 'var(--floating-button-background)'
+			}
+		}
+	},
+	plugins: [require('tailwindcss-animated')]
+};
