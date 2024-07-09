e0e069b5792bb7d044f76847c2522687e10a2f1f
4lysson-a <4ly.alcantara@gmail.com>
2024-07-08 23:37:06 -0300

    feat: Add NewFeatureBadge component for displaying new feature status

---
+++ frontend/src/components/shared/Badges/NewFeature/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NewFeatureBadge() {
+  return (
+    <div className="absolute top-0 left-0 h-3 w-3 rounded-full bg-yellow-600 animate-ping" />
+  );
+}
