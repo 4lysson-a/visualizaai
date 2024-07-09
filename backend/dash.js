8d397d4011dfaefcdf561450fd47290a406ed2e8
4lysson-a <4ly.alcantara@gmail.com>
2024-07-08 23:55:03 -0300

    chore: Add backend files for dashboard, deployment, and server configuration

---
+++ backend/dash.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const { exec } = require('child_process');
+
+const dashboardCommand = `npx parse-dashboard --dev --appId ${process.env.APPLICATION_ID} --masterKey ${process.env.MASTER_KEY} --serverURL "http://localhost:1337/parse"`;
+exec(dashboardCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
