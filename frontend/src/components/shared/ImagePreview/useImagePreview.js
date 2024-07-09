3bb8309d24a4d6b3a622fbf593cfeaac69f127f8
4lysson-a <4ly.alcantara@gmail.com>
2024-07-08 23:37:00 -0300

    chore: Add ImagePreview component for image preview functionality

---
+++ frontend/src/components/shared/ImagePreview/useImagePreview.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function useImagePreview() {
+	const backdropRef = React.useRef(null);
+
+	const [expanded, setExpanded] = React.useState(false);
+
+	const handleOpen = () => {
+		if (expanded) {
+			return;
+		}
+
+		setExpanded(true);
+	};
+
+	const handleClose = () => {
+		setExpanded(false);
+	};
+
+	React.useEffect(() => {
+		function handleClickOutside(event) {
+			if (backdropRef.current && !backdropRef.current.contains(event.target)) {
+				handleClose();
+			}
+		}
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [backdropRef]);
+
+  return {
+    backdropRef,
+    expanded,
+    handleOpen,
+    handleClose
+  }
+}
