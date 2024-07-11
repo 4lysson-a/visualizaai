a47525f936b27cf6d913ddb9beea4a5bd6513661
4lysson-a <4ly.alcantara@gmail.com>
2024-07-11 03:17:30 -0300

    refactor: Update layout.tsx to import animations.css for improved page styling

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import '@/styles/globals.css';
+import '@/styles/animations.css';
+
 import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
