aff6db8512d55f1a340fdee3f03016a4a48d11aa
4lysson-a <4ly.alcantara@gmail.com>
2024-08-24 17:14:03 -0300

    refactor(frontend): Update Clip page with Phone component and separators

--- frontend/src/pages/Dash/Clip/index.jsx
+++ frontend/src/pages/Dash/Clip/index.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import PageTemplate from '@/pages/Dash/lib/PageTemplate';
+import PageTemplate from "@/pages/Dash/lib/PageTemplate";
 
-import LinkMenu from '@/pages/Dash/Clip/LinkMenu';
-import QrCode from '@/pages/Dash/Clip/QrCode';
+import LinkMenu from "@/pages/Dash/Clip/LinkMenu";
+import QrCode from "@/pages/Dash/Clip/QrCode";
 
-import Separator from '@/components/shared/Separator';
-import DownloadMenu from './DownloadMenu';
-import Category from './Category';
+import Separator from "@/components/shared/Separator";
+import DownloadMenu from "./DownloadMenu";
+import Category from "./Category";
+import Phone from "./Phone";
 
 export default function Clip() {
   return (
-    <PageTemplate title={{ text: 'Gerenciar', highlight: 'Opções' }}>
+    <PageTemplate title={{ text: "Gerenciar", highlight: "Opções" }}>
+      <Phone />
+      <Separator />
       <Category />
       <Separator />
       <LinkMenu />
