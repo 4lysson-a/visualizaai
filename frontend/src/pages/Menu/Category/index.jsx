5d3a46c88aa3e13a88730137f768c31e53bf5141
4lysson-a <4ly.alcantara@gmail.com>
2024-08-28 07:27:42 -0300

    chore(frontend): add Category component for menu page

---
+++ frontend/src/pages/Menu/Category/index.jsx
@@ -0,0 +1,46 @@
+import ScrollShadow from "@/components/shared/ScrollShadow";
+import useMenu from "@/hooks/zustand/(public)/useMenu";
+import { sty } from "@/utils";
+import React from "react";
+
+const Item = ({ name }) => {
+    const [filter, setFilter] = useMenu(s => [s.filter, s.setFilter]);
+
+    const link = `#${name}`;
+
+    const handleClick = () => {
+        setFilter({ ...filter, category: name?.trim() });
+    };
+
+    return (
+        <a
+            href={link}
+            onClick={handleClick}
+            className={sty("text-lg font-bold text-nowrap", filter.category === name && "text-primary font-bold")}
+        >
+            {name}
+        </a>
+    );
+};
+
+export default function Categories() {
+    const menu = useMenu(s => s.menu);
+
+    if (menu?.categories) {
+        return (
+            <ScrollShadow
+                variant="horizontal"
+                color="var(--background)"
+                className=" border-b border-smooth-contrast sticky top-0 z-40 items-center flex-row flex w-full  "
+            >
+                <div className="overflow-auto bg-background hide-scroll flex flex-row gap-5 pr-5 p-2 pl-5">
+                    {menu?.categories?.items?.map(category => (
+                        <Item key={category.id} name={category?.get("name")} />
+                    ))}
+                </div>
+            </ScrollShadow>
+        );
+    }
+
+    return null;
+}
