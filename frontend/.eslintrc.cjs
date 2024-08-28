9cc0a6369288d72a01767e19765e7aa4f7d4c36f
4lysson-a <4ly.alcantara@gmail.com>
2024-08-28 09:31:04 -0300

    refact: finish formatter settings

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,34 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        'react/prop-types': 'off',
-        'no-inner-declarations': 'off',
+    plugins: ["react"],
+    rules: {
+        "react/prop-types": "off",
+        "no-inner-declarations": "off"
     }
-}
+};
