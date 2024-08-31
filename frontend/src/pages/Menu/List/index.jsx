1afed711b03bfc02b61a5f12664a4a8a70693854
4lysson-a <4ly.alcantara@gmail.com>
2024-08-31 19:19:56 -0300

    feat: Update Dash component to include additional user and company information

--- frontend/src/pages/Menu/List/index.jsx
+++ frontend/src/pages/Menu/List/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Product from "@/components/shared/Product";
 import useMenu from "@/hooks/zustand/(public)/useMenu";
 import { Fragment } from "react";
+import { debounce } from "lodash";
 
 const BLANK_CATEGORY_ID = "Sem categoria";
 
@@ -25,7 +26,7 @@ export default function List() {
     const processedCategories = new Set();
 
     React.useEffect(() => {
-        const handleScroll = () => {
+        const handleScroll = debounce(() => {
             for (let i = 0; i < productRefs.current.length; i++) {
                 const productElement = productRefs.current[i];
                 if (productElement) {
@@ -40,12 +41,10 @@ export default function List() {
                     }
                 }
             }
-        };
+        }, 200);
 
         window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+        return () => window.removeEventListener("scroll", handleScroll);
     }, [validProducts]);
 
     return validProducts?.map((product, index) => {
