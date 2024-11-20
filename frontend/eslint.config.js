4c0dbee1bdd7bbb99bd213fdbb4f249fef385243
4lysson-a <4ly.alcantara@gmail.com>
2024-11-20 17:05:31 -0300

    refact(frontend): enhance ESLint configuration with additional React rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -12,7 +12,40 @@ export default [
     rules: {
       'react/prop-types': 'off',
       'indent': ['error', 2],
-      'quotes': ['error', 'single']
+      'quotes': ['error', 'single'],
+      'react/prefer-stateless-function': 'error',
+      'react/button-has-type': 'error',
+      'react/no-unused-prop-types': 'error',
+      'react/jsx-pascal-case': 'error',
+      'react/jsx-no-script-url': 'error',
+      'react/no-children-prop': 'error',
+      'react/no-danger': 'error',
+      'react/no-danger-with-children': 'error',
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-fragments': 'error',
+      'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
+      'react/jsx-max-depth': ['error', { max: 5 }],
+      'react/function-component-definition': [
+        'warn',
+        { namedComponents: 'arrow-function' },
+      ],
+      'react/jsx-key': [
+        'error',
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'react-refresh/only-export-components': 'warn',
+      'react/no-typos': 'warn',
+      'react/display-name': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-sort-props': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-one-expression-per-line': 'off',
     }
   }
 ];
