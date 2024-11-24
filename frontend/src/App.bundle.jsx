ee9d38c587a970c4a4bef15d9c767aad7190b7cb
4lysson-a <4ly.alcantara@gmail.com>
2024-11-24 18:16:16 -0300

    style: standardize quotation marks and improve code formatting in App.bundle.jsx

--- frontend/src/App.bundle.jsx
+++ frontend/src/App.bundle.jsx
@@ -1,31 +1,31 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 
-import Loading from './components/shared/Loading';
+import Loading from "./components/shared/Loading";
 
-const Router = React.lazy(() => import('./router'));
+const Router = React.lazy(() => import("./router"));
 
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import useChangeTheme from './hooks/useChangeTheme';
-import LogoLoading from './components/shared/Loading/Logo';
-import useValidateNewVersion from './hooks/useValidateNewVersion';
-import posthog from 'posthog-js';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import useChangeTheme from "./hooks/useChangeTheme";
+import LogoLoading from "./components/shared/Loading/Logo";
+import useValidateNewVersion from "./hooks/useValidateNewVersion";
+import posthog from "posthog-js";
 
 function App() {
-  useValidateNewVersion();
-  useChangeTheme();
+    useValidateNewVersion();
+    useChangeTheme();
 
-  posthog.capture('$pageview');
+    posthog.capture("$pageview");
 
-  return (
-    <>
-      <LogoLoading />
-      <Suspense fallback={<Loading />}>
-        <Router />
-      </Suspense>
-      <ToastContainer autoClose={1500} />
-    </>
-  );
+    return (
+        <>
+            <LogoLoading />
+            <Suspense fallback={<Loading />}>
+                <Router />
+            </Suspense>
+            <ToastContainer autoClose={1500} />
+        </>
+    );
 }
 
 export default App;
