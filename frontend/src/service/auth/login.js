17d9df71149c5061569682458da33045fdecd63d
4lysson-a <4ly.alcantara@gmail.com>
2024-12-02 21:26:27 -0300

    refactor(auth): remove commented-out subscription validation code from login function

--- frontend/src/service/auth/login.js
+++ frontend/src/service/auth/login.js
@@ -3,12 +3,6 @@ import { AppParse } from '@/service/Parse';
 export const login = async (username, password) => {
   const user = await AppParse.User.logIn(username, password);
 
-  // const validateIfUserSubscriptionIsActive = await AppParse.Cloud.run('isSubscriptionActive', {
-  // 	userId: user.id
-  // });
-
-  // console.log(validateIfUserSubscriptionIsActive);
-
   return {
     ...user
   };
