d9f07de06188eaaec39438f5ceff830727c438ee
4lysson-a <4ly.alcantara@gmail.com>
2024-12-02 21:24:43 -0300

    feat(auth): integrate Auth0 for user validation and add master key helper

--- backend/cloud/controllers/auth/auth0.js
+++ backend/cloud/controllers/auth/auth0.js
@@ -1,38 +1,32 @@
-Parse.Cloud.define("auth0", async (request) => {
-    const axios = require('axios');
+Parse.Cloud.define("validateUser", async (request) => {
+  const token = request.params.auth;
 
-    console.log('oi')
+  if (!token) {
+    throw new Error("auth0 is required");
+  }
 
-    const { code } = request.params;
-    const auth0ClientID = process.env.AUTH0_CLIENT_ID;
-    const auth0ClientSecret = process.env.AUTH0_CLIENT_SECRET;
-    const auth0Domain = process.env.AUTH0_DOMAIN;
+  try {
+    const query = new Parse.Query(Parse.User);
+    query.equalTo("auth0", token);
 
-    // Realiza a troca do código de autorização por tokens
-    try {
-        const response = await axios.post(`https://${auth0Domain}/oauth/token`, {
-            client_id: auth0ClientID,
-            client_secret: auth0ClientSecret,
-            code: code,
-            redirect_uri: 'SUA_REDIRECIONAMENTO_URI',
-            grant_type: 'authorization_code',
-        });
+    const user = await query.first({
+      useMasterKey: true,
+    });
 
-        const { access_token, id_token } = response.data;
-
-        // Decodifica o id_token para obter as informações do usuário
-        const jwt = require('jsonwebtoken');
-        const decoded = jwt.decode(id_token);
-
-        if (decoded.email_verified) {
-            // Se o e-mail foi verificado, cria o usuário no seu sistema
-            await createUserInSystem(decoded);
-            return { success: true, message: "Usuário criado com sucesso!" };
-        } else {
-            return { success: false, message: "O e-mail do usuário não foi verificado." };
-        }
-
-    } catch (error) {
-        return { success: false, message: error.message };
+    if (!user) {
+      return {
+        state: "CREATE_ACCOUNT",
+      };
     }
+
+    const newKey = [...Array(22)]
+      .map(() => Math.random().toString(36)[2])
+      .join("");
+    user.set("password", newKey);
+    await user.save(null, { useMasterKey: true });
+
+    return { state: "ACCOUNT_VALIDATED", key: newKey };
+  } catch (error) {
+    throw new Error("Error fetching user: " + error.message);
+  }
 });
