ec2c92505340511edbc31734f25a4266d8fe53e5
4lysson-a <4ly.alcantara@gmail.com>
2024-12-03 01:17:02 -0300

    refactor(frontend): improve formatting and styling in EmailValidation component

--- frontend/src/pages/Auth/Validation/Email/index.jsx
+++ frontend/src/pages/Auth/Validation/Email/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Lottie from 'lottie-react'
-import BackButton from '@/components/shared/BackButton'
+import React from 'react';
+import Lottie from 'lottie-react';
+import BackButton from '@/components/shared/BackButton';
 import emailAnimation from '@/assets/animations/email.json';
 
 import { GmailBtn, ReloadBtn } from './buttons';
 import { useAuth0 } from '@auth0/auth0-react';
 
-export default function EmailValidation(){
+export default function EmailValidation() {
   const { logout } = useAuth0();
 
   return (
@@ -16,15 +16,18 @@ export default function EmailValidation(){
       <div className="flex flex-col gap-4">
         <Lottie animationData={emailAnimation} />
         <h1 className="text-2xl font-medium">
-                        Para finalizar a criação da sua conta <span className="text-primary">valide seu email</span>
+                    Para finalizar a criação da sua conta <span className="text-primary">valide seu email</span>
         </h1>
-        <p className='text-lg'>Após finalizar a validação volte nessa tela e clique no botão de recarregar, ou apenas faça login novamente no site</p>
+        <p className="text-lg">
+                    Após finalizar a validação volte nessa tela e clique no botão de recarregar, ou apenas faça login
+                    novamente no site
+        </p>
       </div>
 
-      <div className='flex gap-4'>
+      <div className="flex gap-4">
         <GmailBtn />
         <ReloadBtn />
       </div>
     </div>
-  )
+  );
 }
